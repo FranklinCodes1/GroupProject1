b600346a5d0edecdb950a2da0c30b503ad216639
Mandell Butler <mandell.butler@outlook.com>
2021-04-28 14:19:42 -0400

    added id's to input fields

--- assets/script.js
+++ assets/script.js
@@ -5,5 +5,12 @@ searchBtn.click(conductSearch)
 
 function conductSearch(event) {
  event.preventDefault();
+    //create
+ var userStart = document.getElementById("startPoint")
+ var userEnd = document.getElementById("endPoint")
+ var userInterest = document.getElementById("interestPoint")
+
+    //build
+
 console.log("This shit here works!")
 }
